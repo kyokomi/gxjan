376577e9c7e6c1870af72429f576fbc142b69d57
kyokomi <kyoko1220adword@gmail.com>
2015-03-25 13:27:03 +0900

    とりあえず配牌表示まで

---
+++ main.go
@@ -0,0 +1,101 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/math"
+	"github.com/google/gxui/themes/dark"
+
+	"image"
+	"log"
+	"os"
+	"github.com/kyokomi/gomajan/pai"
+	"github.com/kyokomi/gomajan/taku"
+)
+
+var data = flag.String("data", "", "path to data")
+
+type MainWindow struct {
+	gxui.Window
+	theme gxui.Theme
+	driver gxui.Driver
+}
+
+func NewMainWindow(driver gxui.Driver) MainWindow {
+	var mainWindow MainWindow
+	theme := dark.CreateTheme(driver)
+	window := theme.CreateWindow(800, 600, "Polygon")
+
+	mainWindow.Window = window
+	mainWindow.theme = theme
+	mainWindow.driver = driver
+
+	return mainWindow
+}
+
+func appMain(driver gxui.Driver) {
+
+	window := NewMainWindow(driver)
+
+	taku := taku.NewTaku()
+
+	containerBase := window.Theme().CreateLinearLayout()
+	containerBase.SetOrientation(gxui.Vertical)
+	for _, player := range taku.Players {
+		container := window.Theme().CreateLinearLayout()
+		container.SetOrientation(gxui.Horizontal)
+		for _, hai := range player.Tiles() {
+			if hai.Pai.Type() == pai.NoneType || hai.Val <= 0 {
+				continue
+			}
+			for i := 0; i < hai.Val; i++ {
+				container.AddChild(window.createPaiImage(hai.Pai))
+			}
+		}
+		containerBase.AddChild(container)
+	}
+
+//	label := window.Theme().CreateLabel()
+//	label.SetText("hogehogehoge")
+//	label.SetMargin(math.CreateSpacing(200))
+//	container.AddChild(label)
+	window.AddChild(containerBase)
+	window.OnClose(driver.Terminate)
+	gxui.EventLoop(driver)
+}
+
+func (m MainWindow) Theme() gxui.Theme {
+	return m.theme
+}
+
+func (m MainWindow) createPaiImage(p pai.MJP) gxui.Image {
+	f, err := os.Open("data/pai-images/" + imageMappings[p])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	im, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	pict := m.theme.CreateImage()
+	texture := m.driver.CreateTexture(im, 96)
+	texture.SetFlipY(true)
+	pict.SetTexture(texture)
+	pict.SetExplicitSize(math.Size{32, 45})
+	//	pict.SetMargin(math.CreateSpacing(4))
+
+	return pict
+}
+
+func main() {
+	flag.Parse()
+	gl.StartDriver(*data, appMain)
+}
---
+++ mapping.go
@@ -0,0 +1,43 @@
+package main
+
+import "github.com/kyokomi/gomajan/pai"
+
+var imageMappings = map[pai.MJP]string{
+	pai.TON: "ji1-66-90-l-emb.png",
+	pai.NAN: "ji2-66-90-l-emb.png",
+	pai.SHA: "ji3-66-90-l-emb.png",
+	pai.PEI: "ji4-66-90-l-emb.png",
+	pai.HAK: "ji5-66-90-l-emb.png",
+	pai.HAT: "ji6-66-90-l-emb.png",
+	pai.CHN: "ji7-66-90-l-emb.png",
+
+	pai.M1: "man1-66-90-l-emb.png",
+	pai.M2: "man2-66-90-l-emb.png",
+	pai.M3: "man3-66-90-l-emb.png",
+	pai.M4: "man4-66-90-l-emb.png",
+	pai.M5: "man5-66-90-l-emb.png",
+	pai.M6: "man6-66-90-l-emb.png",
+	pai.M7: "man7-66-90-l-emb.png",
+	pai.M8: "man8-66-90-l-emb.png",
+	pai.M9: "man9-66-90-l-emb.png",
+
+	pai.S1: "sou1-66-90-l-emb.png",
+	pai.S2: "sou2-66-90-l-emb.png",
+	pai.S3: "sou3-66-90-l-emb.png",
+	pai.S4: "sou4-66-90-l-emb.png",
+	pai.S5: "sou5-66-90-l-emb.png",
+	pai.S6: "sou6-66-90-l-emb.png",
+	pai.S7: "sou7-66-90-l-emb.png",
+	pai.S8: "sou8-66-90-l-emb.png",
+	pai.S9: "sou9-66-90-l-emb.png",
+
+	pai.P1: "pin1-66-90-l-emb.png",
+	pai.P2: "pin2-66-90-l-emb.png",
+	pai.P3: "pin3-66-90-l-emb.png",
+	pai.P4: "pin4-66-90-l-emb.png",
+	pai.P5: "pin5-66-90-l-emb.png",
+	pai.P6: "pin6-66-90-l-emb.png",
+	pai.P7: "pin7-66-90-l-emb.png",
+	pai.P8: "pin8-66-90-l-emb.png",
+	pai.P9: "pin9-66-90-l-emb.png",
+}
